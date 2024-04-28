9a79127a8c2560e472972746b6abf1e3a5b2f71a
Elhackerlibre <elhackerlibre@gmail.com>
2024-04-28 11:44:14 -0400

    Actualizar configuración de Neovim

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,5 +4,7 @@ return {
   keys = {
     {"<A>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
     {"<A>gl", ":Git pull<cr>", desc = "Git pull"},
+    {"<A>gp", ":Git push<cr>", desc = "Git push"},
+
   },
 }
