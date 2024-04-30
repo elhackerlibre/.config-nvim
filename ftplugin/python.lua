a1d1ff3fd55920106d9512b1cc29d4ef7fcbd08f
Elhackerlibre <elhackerlibre@gmail.com>
2024-04-30 16:43:34 -0400

    Nueva actualizacion

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -14,3 +14,26 @@ vim.cmd('filetype plugin indent on') -- Activar detección de tipo de archivo
 
 -- Mapeo de teclas con <F%> para abrir un terminal horizontal en la parte inferior y ejecutar Python
 map("n", "<F5>", ":belowright split term://python3 %<CR>", { noremap = true })
+
+
+
+-- Definir una función para crear mapeos de teclas
+--local function map(mode, lhs, rhs, opts)
+--  local options = { noremap = true, silent = true }
+--  if opts then
+--    options = vim.tbl_extend("force", options, opts)
+--  end
+--  vim.keymap.set(mode, lhs, rhs, options)
+--end
+
+-- Función para abrir un terminal horizontal en la parte inferior con un tamaño personalizado
+-- local function open_terminal()
+--  local width = vim.o.columns * 0.8 -- 80% del ancho de la ventana
+--  local height = vim.o.lines * 0.4 -- 40% de la altura de la ventana
+--  vim.cmd(string.format(":belowright split | resize %d", height))
+--  vim.cmd("term python3 %")
+-- end
+
+-- Mapeo de teclas para abrir la terminal personalizada
+-- map("n", "<F5>", ":lua open_terminal()<CR>", { noremap = true })
+
